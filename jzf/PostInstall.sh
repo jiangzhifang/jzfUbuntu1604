d0f77f78c3ebb621b9271f1aec977a9b6d51c950
jiangzhifang <931430472@qq.com>
2020-07-23 14:58:11 +0800

    test2

--- jzf/PostInstall.sh
+++ jzf/PostInstall.sh
@@ -3,5 +3,11 @@
 # File name: PostInstall.sh
 #
 
-mkdir /root//jzfUbuntu1604/jzf/aaaa
-touch /root//jzfUbuntu1604/jzf/aaaa/bbbb
+cp /root/jzfUbuntu1604/jzf/tbclimits.conf /etc/security/limits.d/
+cp /root/jzfUbuntu1604/jzf/99-tbcsysctl.conf /etc/sysctl.d/
+cp /etc/apt/sources.list /etc/apt/sources.list.bak
+cp /root/jzfUbuntu1604/jzf/sources.list /etc/apt/
+sudo wget -O - http://hwraid.le-vert.net/debian/hwraid.le-vert.net.gpg.key | sudo apt-key add -
+sudo apt-get update
+sudo apt-get -y install megacli
+
